88d368d38c5d3cea9cfb5a4b0cfccc27bb264053
Andrea Ostuni <andrea.ostuni@polito.it>
2023-05-19 16:28:36 +0000

    add component sgm_gpu_launch

--- launch/sgm_gpu_test.launch.py
+++ launch/sgm_gpu_test.launch.py
@@ -17,12 +17,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='sgm_gpu',
-            node_executable='sgm_gpu_node',
-            node_name='sgm_gpu',
+            executable='sgm_gpu_node',
+            name='sgm_gpu',
             remappings=[
                 ('left_image', '/test_stereo_publisher/left/image_raw'),
                 ('right_image', '/test_stereo_publisher/right/image_raw'),
@@ -32,29 +33,28 @@ def generate_launch_description():
         ),
         Node(
             package='sgm_gpu',
-            node_executable='test_stereo_publisher',
-            node_name='test_stereo_publisher'
+            executable='test_stereo_publisher',
+            name='test_stereo_publisher'
         ),
         Node(
             package='image_view',
-            node_executable='disparity_view',
-            node_name='disparity_view',
+            executable='disparity_view',
+            name='disparity_view',
             remappings=[('image', '/sgm_gpu/disparity')],
             parameters=[{'window_name': 'Disparity'}]
         ),
         Node(
             package='image_view',
-            node_executable='image_view',
-            node_name='left_view',
+            executable='image_view',
+            name='left_view',
             remappings=[('image', '/test_stereo_publisher/left/image_raw')],
             parameters=[{'window_name': 'Left image'}]
         ),
         Node(
             package='image_view',
-            node_executable='image_view',
-            node_name='left_view',
+            executable='image_view',
+            name='left_view',
             remappings=[('image', '/test_stereo_publisher/right/image_raw')],
             parameters=[{'window_name': 'Right image'}]
         )
     ])
-
